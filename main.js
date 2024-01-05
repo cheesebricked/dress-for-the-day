e8e5d6e6ffb3ccf3a99668d709814c4372eeaf78
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-01-04 22:08:34 -0800

    added break to season switch case

--- main.js
+++ main.js
@@ -116,16 +116,19 @@ function getSeason() {
         case 0:
         case 1:
             return "winter";
+            break;
         
         case 2:
         case 3:
         case 4:
             return "spring";
+            break;
 
         case 5:
         case 6:
         case 7:
             return "summer";
+            break;
 
         case 8:
         case 9:
