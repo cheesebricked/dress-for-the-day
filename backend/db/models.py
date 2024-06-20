9a5fd91e565894a90dde69978ba3af2357cdb159
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-06-19 21:51:55 -0700

    Fixe user db password being unique

--- backend/db/models.py
+++ backend/db/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)      
     username = db.Column(db.String(80), unique = False, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(80), unique = True, nullable = False)
+    password = db.Column(db.String(80), unique = False, nullable = False)
     #likes = db.relationship('Like', backref='owner')
 
     def to_json(self):
